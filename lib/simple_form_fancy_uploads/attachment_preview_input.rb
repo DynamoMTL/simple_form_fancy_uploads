096ffb7d4f0d54077c0c24b5cf8ca1076aac1b63
Hugo Frappier <hugo@cekoya.com>
2012-04-17 15:15:28 -0400

    Support Dragonfly.
    Need to change the specs as well.

--- lib/simple_form_fancy_uploads/attachment_preview_input.rb
+++ lib/simple_form_fancy_uploads/attachment_preview_input.rb
@@ -2,8 +2,25 @@ module SimpleFormFancyUploads
   class AttachmentPreviewInput < SimpleForm::Inputs::FileInput
     def input
       out = ''
-      if object.send("#{attribute_name}?")
-        out << template.link_to(object.send(attribute_name).filename, object.send(attribute_name).url)
+
+      if object.respond_to? "#{attribute_name}?"
+        has_attachment = object.send("#{attribute_name}?")
+      else
+        has_attachment = object.send("#{attribute_name}").present?
+      end
+
+      if has_attachment
+        attachment = object.send("#{attribute_name}")
+        url = attachment.url
+
+        if attachment.respond_to? :filename
+          filename = attachment.filename
+        elsif attachment.respond_to? :name
+          filename = attachment.name
+        else
+          filename = File.basename(url)
+        end
+        out << template.link_to(filename, url)
       end
       (out << super).html_safe
     end
--- lib/simple_form_fancy_uploads/image_preview_input.rb
+++ lib/simple_form_fancy_uploads/image_preview_input.rb
@@ -5,8 +5,31 @@ module SimpleFormFancyUploads
       use_default_url = options.delete(:use_default_url) || false
 
       out = ''
-      if object.send("#{attribute_name}?") || use_default_url
-        out << template.image_tag(object.send(attribute_name).tap {|o| break o.send(version) if version}.send('url'))
+
+      if object.respond_to? "#{attribute_name}?"
+        has_attachment = object.send("#{attribute_name}?")
+      else
+        has_attachment = object.send("#{attribute_name}").present?
+      end
+
+      if has_attachment
+        attribute = object.send(attribute_name)
+        image = attribute
+
+        if version
+          if attribute.respond_to? :thumb
+            image = attribute.send(:thumb, version)
+          elsif attribute.respond_to version
+            image = attribute.send(version)
+          end
+        end
+
+        if defined?(Dragonfly)
+        end
+
+        if attribute || use_default_url
+          out << template.image_tag(image.send('url'))
+        end
       end
       (out << super).html_safe
     end
